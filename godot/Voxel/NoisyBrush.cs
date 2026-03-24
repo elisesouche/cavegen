63703d7b7f0a37114e6739c95c46e17331167ab8
Elfahor <elfahor@gmx.fr>
2026-03-24 18:06:45 +0100

    fix noisy brush it works now!

--- godot/Voxel/NoisyBrush.cs
+++ godot/Voxel/NoisyBrush.cs
@@ -17,14 +17,7 @@ public partial class NoisyBrush : Brush
 
     public override Vector3 Bounds => new(Density.MaxDomain, Density.MaxDomain, Density.MaxDomain);
 
-    public override float GetValueAtLocal(Vector3 offset)
-    {
-        var v = Mathf.Clamp(
-            Density.SampleBaked(offset.DistanceTo(Position))
-                + NoiseStrength * Mathf.Remap(Noise.GetNoise3Dv(offset), -1, 1, 0, 1),
-            0,
-            1
-        );
-        return v;
-    }
+    public override float GetValueAtLocal(Vector3 offset) =>
+        Density.SampleBaked(offset.DistanceTo(Position))
+        + Mathf.Remap(Noise.GetNoise3Dv(ToGlobal(offset)), -1, 1, -NoiseStrength, 0);
 }
